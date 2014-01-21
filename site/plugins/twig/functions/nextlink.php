6eabe59e6300e729288505931061af07e70d5ba8
Thomas Breuss <>
2014-01-21 07:38:40 +0100

    Added licence information

--- site/plugins/twig/functions/nextlink.php
+++ site/plugins/twig/functions/nextlink.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Herbie.
+ *
+ * (c) Thomas Breuss <www.tebe.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 return new Twig_SimpleFunction('nextlink', function ($route, $label) {
     $url = $this['urlGenerator']->generate($route);
     return sprintf('<p class="pagination"><a href="%s" class="pure-button">%s<i class="fa fa-arrow-right"></i></a></p>', $url, $label);
