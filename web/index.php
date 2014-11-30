f6d04ad767c665ed1de8d99c032f7fe209d9af94
Thomas Breuss <thomasbreuss@gmx.ch>
2014-11-30 14:40:34 +0100

    Using initializer and debug constant

--- web/index.php
+++ web/index.php
@@ -13,7 +13,11 @@ ini_set('display_errors', 1);
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
+define('HERBIE_DEBUG', true);
+
 $app = new Herbie\Application('../site');
+
+$app->init();
 
 $app['config']->set('plugins.shortcode.box1', function($atts, $content) {
     return '<div class="pure-u-1-2 box box-1"><div markdown="1">'
